b83302beb769830fb52ac9a8be5a1254e430c294
Ricardo Arias <ricardoariassalazarc@outlook.com>
2025-12-13 10:39:17 -0600

    Implementación de apiSlice.js

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,13 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./features/auth/authSlice";
-import carritoReducer from "./features/carrito/carritoSlice";
-import { apiSlice } from "./api/apiSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import authReducer from '../features/auth/authSlice';
+import { apiSlice } from '../features/api/apiSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    carrito: carritoReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
-  middleware: (getDefault) => getDefault().concat(apiSlice.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
